6be674fb234c9033e02796f400e05d1d1c265cc3
murata0531 <56902467+murata0531@users.noreply.github.com>
2021-02-15 05:14:12 +0900

    Add files via upload
    ok

---
+++ csg040_GradeCheck/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace csg040_GradeCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxAttendanceM = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelResultM = new System.Windows.Forms.Label();
+            this.textBoxScoreM = new System.Windows.Forms.TextBox();
+            this.labelCompAvgM = new System.Windows.Forms.Label();
+            this.labelCompAvgP = new System.Windows.Forms.Label();
+            this.textBoxScoreP = new System.Windows.Forms.TextBox();
+            this.labelResultP = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxAttendanceP = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labelCompAvgE = new System.Windows.Forms.Label();
+            this.textBoxScoreE = new System.Windows.Forms.TextBox();
+            this.labelResultE = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBoxAttendanceE = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.buttonJudge = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "科目";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "出席率";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(256, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "得点";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(389, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "判定結果";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(494, 45);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "平均点と比較";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 12);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "数学&M)";
+            // 
+            // textBoxAttendanceM
+            // 
+            this.textBoxAttendanceM.Location = new System.Drawing.Point(103, 80);
+            this.textBoxAttendanceM.Name = "textBoxAttendanceM";
+            this.textBoxAttendanceM.Size = new System.Drawing.Size(100, 19);
+            this.textBoxAttendanceM.TabIndex = 6;
+            this.textBoxAttendanceM.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(209, 83);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(11, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "%";
+            // 
+            // labelResultM
+            // 
+            this.labelResultM.AutoSize = true;
+            this.labelResultM.Location = new System.Drawing.Point(389, 86);
+            this.labelResultM.Name = "labelResultM";
+            this.labelResultM.Size = new System.Drawing.Size(35, 12);
+            this.labelResultM.TabIndex = 8;
+            this.labelResultM.Text = "label8";
+            // 
+            // textBoxScoreM
+            // 
+            this.textBoxScoreM.Location = new System.Drawing.Point(258, 80);
+            this.textBoxScoreM.Name = "textBoxScoreM";
+            this.textBoxScoreM.Size = new System.Drawing.Size(100, 19);
+            this.textBoxScoreM.TabIndex = 9;
+            this.textBoxScoreM.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelCompAvgM
+            // 
+            this.labelCompAvgM.AutoSize = true;
+            this.labelCompAvgM.Location = new System.Drawing.Point(494, 87);
+            this.labelCompAvgM.Name = "labelCompAvgM";
+            this.labelCompAvgM.Size = new System.Drawing.Size(35, 12);
+            this.labelCompAvgM.TabIndex = 10;
+            this.labelCompAvgM.Text = "label8";
+            // 
+            // labelCompAvgP
+            // 
+            this.labelCompAvgP.AutoSize = true;
+            this.labelCompAvgP.Location = new System.Drawing.Point(494, 138);
+            this.labelCompAvgP.Name = "labelCompAvgP";
+            this.labelCompAvgP.Size = new System.Drawing.Size(35, 12);
+            this.labelCompAvgP.TabIndex = 16;
+            this.labelCompAvgP.Text = "label8";
+            // 
+            // textBoxScoreP
+            // 
+            this.textBoxScoreP.Location = new System.Drawing.Point(258, 131);
+            this.textBoxScoreP.Name = "textBoxScoreP";
+            this.textBoxScoreP.Size = new System.Drawing.Size(100, 19);
+            this.textBoxScoreP.TabIndex = 15;
+            this.textBoxScoreP.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelResultP
+            // 
+            this.labelResultP.AutoSize = true;
+            this.labelResultP.Location = new System.Drawing.Point(389, 137);
+            this.labelResultP.Name = "labelResultP";
+            this.labelResultP.Size = new System.Drawing.Size(35, 12);
+            this.labelResultP.TabIndex = 14;
+            this.labelResultP.Text = "label8";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(209, 134);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(11, 12);
+            this.label10.TabIndex = 13;
+            this.label10.Text = "%";
+            // 
+            // textBoxAttendanceP
+            // 
+            this.textBoxAttendanceP.Location = new System.Drawing.Point(103, 131);
+            this.textBoxAttendanceP.Name = "textBoxAttendanceP";
+            this.textBoxAttendanceP.Size = new System.Drawing.Size(100, 19);
+            this.textBoxAttendanceP.TabIndex = 12;
+            this.textBoxAttendanceP.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(23, 134);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(44, 12);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "物理(&P)";
+            // 
+            // labelCompAvgE
+            // 
+            this.labelCompAvgE.AutoSize = true;
+            this.labelCompAvgE.Location = new System.Drawing.Point(494, 191);
+            this.labelCompAvgE.Name = "labelCompAvgE";
+            this.labelCompAvgE.Size = new System.Drawing.Size(35, 12);
+            this.labelCompAvgE.TabIndex = 22;
+            this.labelCompAvgE.Text = "label8";
+            // 
+            // textBoxScoreE
+            // 
+            this.textBoxScoreE.Location = new System.Drawing.Point(258, 184);
+            this.textBoxScoreE.Name = "textBoxScoreE";
+            this.textBoxScoreE.Size = new System.Drawing.Size(100, 19);
+            this.textBoxScoreE.TabIndex = 21;
+            this.textBoxScoreE.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelResultE
+            // 
+            this.labelResultE.AutoSize = true;
+            this.labelResultE.Location = new System.Drawing.Point(389, 190);
+            this.labelResultE.Name = "labelResultE";
+            this.labelResultE.Size = new System.Drawing.Size(35, 12);
+            this.labelResultE.TabIndex = 20;
+            this.labelResultE.Text = "label8";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(209, 187);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(11, 12);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "%";
+            // 
+            // textBoxAttendanceE
+            // 
+            this.textBoxAttendanceE.Location = new System.Drawing.Point(103, 184);
+            this.textBoxAttendanceE.Name = "textBoxAttendanceE";
+            this.textBoxAttendanceE.Size = new System.Drawing.Size(100, 19);
+            this.textBoxAttendanceE.TabIndex = 18;
+            this.textBoxAttendanceE.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(23, 187);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(44, 12);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "英語(&E)";
+            // 
+            // buttonJudge
+            // 
+            this.buttonJudge.Location = new System.Drawing.Point(197, 266);
+            this.buttonJudge.Name = "buttonJudge";
+            this.buttonJudge.Size = new System.Drawing.Size(89, 32);
+            this.buttonJudge.TabIndex = 23;
+            this.buttonJudge.Text = "判定(&J)";
+            this.buttonJudge.UseVisualStyleBackColor = true;
+            this.buttonJudge.Click += new System.EventHandler(this.buttonJudge_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(304, 266);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(80, 32);
+            this.buttonReset.TabIndex = 24;
+            this.buttonReset.Text = "リセット(&R)";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 339);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonJudge);
+            this.Controls.Add(this.labelCompAvgE);
+            this.Controls.Add(this.textBoxScoreE);
+            this.Controls.Add(this.labelResultE);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBoxAttendanceE);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.labelCompAvgP);
+            this.Controls.Add(this.textBoxScoreP);
+            this.Controls.Add(this.labelResultP);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxAttendanceP);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.labelCompAvgM);
+            this.Controls.Add(this.textBoxScoreM);
+            this.Controls.Add(this.labelResultM);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxAttendanceM);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "成績判定";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxAttendanceM;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelResultM;
+        private System.Windows.Forms.TextBox textBoxScoreM;
+        private System.Windows.Forms.Label labelCompAvgM;
+        private System.Windows.Forms.Label labelCompAvgP;
+        private System.Windows.Forms.TextBox textBoxScoreP;
+        private System.Windows.Forms.Label labelResultP;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxAttendanceP;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labelCompAvgE;
+        private System.Windows.Forms.TextBox textBoxScoreE;
+        private System.Windows.Forms.Label labelResultE;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBoxAttendanceE;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button buttonJudge;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ csg040_GradeCheck/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace csg040_GradeCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // 起動時のイベントハンドラ
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelResultM.Text = "";
+            labelResultP.Text = "";
+            labelResultE.Text = "";
+            labelCompAvgM.Text = "";
+            labelCompAvgP.Text = "";
+            labelCompAvgE.Text = "";
+        }
+
+        // 「判定」ボタンクリックのイベントハンドラ
+        private void buttonJudge_Click(object sender, EventArgs e)
+        {
+            // 出席率を浮動小数点値に変換
+            double AttendanceM, AttendanceP, AttendanceE;
+            textToValue(textBoxAttendanceM.Text, out AttendanceM);
+            textToValue(textBoxAttendanceP.Text, out AttendanceP);
+            textToValue(textBoxAttendanceE.Text, out AttendanceE);
+
+            // 点数を整数値に変換
+            int scoreM, scoreP, scoreE;
+            textToValue(textBoxScoreM.Text, out scoreM);
+            textToValue(textBoxScoreP.Text, out scoreP);
+            textToValue(textBoxScoreE.Text, out scoreE);
+
+            // 点数を判定する
+            labelResultM.Text = scoreJudge(AttendanceM, scoreM);
+            labelResultP.Text = scoreJudge(AttendanceP, scoreP);
+            labelResultE.Text = scoreJudge(AttendanceE, scoreE);
+
+            // 平均値以上か未満か判定する
+            labelCompAvgM.Text = averageJudge(scoreM, 1);
+            labelCompAvgP.Text = averageJudge(scoreP, 2);
+            labelCompAvgE.Text = averageJudge(scoreE, 3);
+        }
+
+        // テキストを浮動小数点値に変換
+        //（仮引数）text：変換する文字列　val：変換した浮動小数点値　
+        //（返却値）なし
+        private void textToValue(string text, out double val)
+        {
+            if (double.TryParse(text, out val) == false)
+                val = -1.0;
+        }
+
+        // テキストを整数値に変換
+        //（仮引数）text：変換する文字列　val：変換した整数値　
+        //（返却値）なし
+        private void textToValue(string text, out int val)
+        {
+            if (int.TryParse(text, out val) == false)
+                val = -1;
+        }
+
+        // 成績判定
+        //（仮引数）Attendance：出席率　score：得点　
+        //（返却値）判定結果
+        private string scoreJudge(double Attendance, int score)
+        {
+            string result;
+
+            if (Attendance < 0.0 || Attendance > 100.0 ||
+                score < 0 || score > 100)
+                result = "エラー";
+            else if (Attendance >= 80.0)
+            {
+                if (score >= 80)
+                    result = "A判定";
+                else if (score >= 70)
+                    result = "B判定";
+                else if (score >= 60)
+                    result = "C判定";
+                else
+                    result = "不合格";
+            }
+            else
+                result = "不合格";
+
+            return result;
+        }
+
+        // 平均点判定
+        //（仮引数）score：得点　subject：科目（1=数学　2=物理　3=英語）　
+        //（返却値）判定結果
+        private string averageJudge(int score, int subject)
+        {
+            string result;
+            int average;
+
+            if (score < 0 || score > 100)
+                return "エラー";
+
+            switch (subject)
+            {
+                case 1:
+                    average = 73;
+                    break;
+                case 2:
+                    average = 65;
+                    break;
+                case 3:
+                    average = 77;
+                    break;
+                default:
+                    return "エラー";
+            }
+
+            if (score >= average)
+                result = "平均点以上";
+            else
+                result = "平均点未満";
+
+            return result;
+        }
+
+        // 「リセット」ボタンクリック時のイベントハンドラ
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            textBoxAttendanceM.Text = "0.0";
+            textBoxAttendanceP.Text = "0.0";
+            textBoxAttendanceE.Text = "0.0";
+            textBoxScoreM.Text = "0";
+            textBoxScoreP.Text = "0";
+            textBoxScoreE.Text = "0";
+            labelResultM.Text = "";
+            labelResultP.Text = "";
+            labelResultE.Text = "";
+            labelCompAvgM.Text = "";
+            labelCompAvgP.Text = "";
+            labelCompAvgE.Text = "";
+        }
+    }
+}
